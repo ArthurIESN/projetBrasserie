a1bb267ec5add4df67c22eaa2d5ad376b46edd02
ArthurIESN <ryckbosch.arthur.iesn@gmail.com>
2025-03-20 16:57:27 +0100

    test db connexion

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,10 +1,9 @@
 package database;
 
 import Config.ConfigReader;
+
+import java.sql.*;
 import java.util.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Database
 {
@@ -16,8 +15,7 @@ public class Database
     {
        // use a mysql database
         String username = ConfigReader.getUsername();
-        String password = ConfigReader.getPassword();
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        String password = "";
         String url = "jdbc:mysql://localhost:3306/brasserie_java_db";
 
         try {
@@ -25,6 +23,19 @@ public class Database
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database");
+
+            // test request get all employees
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
+            System.out.println("Request executed");
+
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString("first_name") + " " + resultSet.getString("last_name"));
+            }
+
+
         } catch (ClassNotFoundException e) {
             System.err.println("MySQL JDBC Driver not found");
             e.printStackTrace();
