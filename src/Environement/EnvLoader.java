81859446f0ede91bcdb60ea7b23a02e61229588d
ArthurIESN <ryckbosch.arthur.iesn@gmail.com>
2025-03-20 20:05:59 +0100

    base

---
+++ src/Environement/EnvLoader.java
@@ -0,0 +1,31 @@
+package Environement;
+
+import java.io.*;
+
+public class EnvLoader
+{
+
+    private final static String filePath = ".env";
+
+    public static String getEnvValue(String key)
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+
+                if (line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+
+                if (line.startsWith(key + "=")) {
+                    return line.substring(key.length() + 1).trim();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
