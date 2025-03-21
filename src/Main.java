23dde59522bfc3d915b80b301da61e9ec6276377
Dorian Michaux <dorianmichaux@MacBook-Pro-de-Dorian.local>
2025-03-21 14:47:21 +0100

    Save local changes before merging

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
-
-        database connection;
+        //database connection;
         Database db = Database.getInstance();
+
     }
 }
