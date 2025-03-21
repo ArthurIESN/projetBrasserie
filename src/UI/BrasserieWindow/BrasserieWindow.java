60f529a9b4e3225d5327f178265c5e3db432f286
ArthurIESN <ryckbosch.arthur.iesn@gmail.com>
2025-03-21 15:22:24 +0100

    ajout main window

--- src/UI/BrasserieWindow/BrasserieWindow.java
+++ src/UI/BrasserieWindow/BrasserieWindow.java
@@ -1,16 +1,39 @@
 package UI.BrasserieWindow;
+package UI.Components.MenuBarBrasserie;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BrasserieWindow extends JFrame
 {
+    private MenuBarBrasserie menuBarBrasserie;
+    private Container container;
+    private JPanel contentPanel;
+
     public BrasserieWindow()
     {
         super("Brasserie");
         setSize(1280, 720);
-        // center windows
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        container = getContentPane();
+        container.setLayout(new BorderLayout());
+
         setVisible(true);
+    }
+
+    private void updateWindowContent(JPanel panel)
+    {
+        if(contentPanel != null)
+        {
+            container.remove(contentPanel);
+        }
+
+        contentPanel = panel;
+        container.add(contentPanel, BorderLayout.CENTER);
+        revalidate();
+        repaint();
     }
 
     public static void main(String[] args)
