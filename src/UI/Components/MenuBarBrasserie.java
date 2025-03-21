3e56cec5c5d6988754aa71e126a3c3fc88fc8e00
ArthurIESN <ryckbosch.arthur.iesn@gmail.com>
2025-03-21 15:31:26 +0100

    add menuBar in BrasserieWindow

--- src/UI/Components/MenuBarBrasserie.java
+++ src/UI/Components/MenuBarBrasserie.java
@@ -1,5 +1,7 @@
 package UI.Components;
 
+import UI.BrasserieWindow.BrasserieWindow;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class MenuBarBrasserie {
     private JMenu recherche;
     private JMenuItem recherche1, recherche2, recherche3;
 
-    public MenuBarBrasserie(){
+    public MenuBarBrasserie(BrasserieWindow brasserieWindow){
         menuBar = new JMenuBar();
 
         recherche = new JMenu("Recherche");
@@ -22,6 +24,24 @@ public class MenuBarBrasserie {
         recherche.add(recherche1);
         recherche.add(recherche2);
         recherche.add(recherche3);
+
+        recherche1.addActionListener(e -> {
+            JPanel panel1 = new JPanel();
+            panel1.add(new JLabel("Recherche1 selected"));
+            brasserieWindow.updateWindowContent(panel1);
+        });
+
+        recherche2.addActionListener(e -> {
+            JPanel panel2 = new JPanel();
+            panel2.add(new JLabel("Recherche2 selected"));
+            brasserieWindow.updateWindowContent(panel2);
+        });
+
+        recherche3.addActionListener(e -> {
+            JPanel panel3 = new JPanel();
+            panel3.add(new JLabel("Recherche3 selected"));
+            brasserieWindow.updateWindowContent(panel3);
+        });
     }
 
     public JMenuBar getMenuBar(){
