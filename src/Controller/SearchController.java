cd332b02cc5692036fabebb1542eb603c65b0333
Dorian Michaux <dorianmichaux@MacBook-Pro-de-Dorian.local>
2025-03-24 08:49:16 +0100

    modif controller

--- src/Controller/SearchController.java
+++ src/Controller/SearchController.java
@@ -1,7 +1,12 @@
 package Controller;
 
 
+import BusinessLogic.Search.SearchItemManager;
 import BusinessLogic.SearchDocumentWithEventManager;
+import Exceptions.DataAccess.DatabaseConnectionFailedException;
+import Exceptions.DataAccess.Search.GetMinMaxItemQuantityAndPriceException;
+import Exceptions.tva.WrongTvaCodeException;
+import Model.Item.Item;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,6 +15,7 @@ import java.util.List;
 public class SearchController {
     private static final SearchController instance = new SearchController();
     private final SearchDocumentWithEventManager searchDocumentWithEventManager = new SearchDocumentWithEventManager();
+    private final SearchItemManager searchItemManager = new SearchItemManager();
 
     public SearchController(){}
 
@@ -20,5 +26,15 @@ public class SearchController {
 
     public static SearchController getInstance(){
         return instance;
+    }
+
+    public ArrayList<Item> searchItem(String tvaCode, int minItem, int maxItem, int minPrice, int maxPrice) throws DatabaseConnectionFailedException, WrongTvaCodeException
+    {
+        return searchItemManager.searchItem(tvaCode, minItem, maxItem, minPrice, maxPrice);
+    }
+
+    public int[] getMinMaxItemQuantityAndPrice() throws DatabaseConnectionFailedException, GetMinMaxItemQuantityAndPriceException
+    {
+        return searchItemManager.getMinMaxItemQuantityAndPrice();
     }
 }
