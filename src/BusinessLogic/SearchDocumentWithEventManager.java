97897e7e504b4d71cfa6fdb85a400a2894535f7c
ArthurIESN <145301410+ArthurIESN@users.noreply.github.com>
2025-03-24 13:47:16 +0100

    Resolve conflitcts

---
+++ src/BusinessLogic/SearchDocumentWithEventManager.java
@@ -0,0 +1,24 @@
+package BusinessLogic;
+
+import DataAccess.Search.SearchDocumentWithEventDBAccess;
+import Exceptions.DataAccess.DatabaseConnectionFailedException;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class SearchDocumentWithEventManager {
+    private final SearchDocumentWithEventDBAccess searchDocumentWithEventDBAccess = new SearchDocumentWithEventDBAccess();
+    public List<Integer> getDatesEvents() throws DatabaseConnectionFailedException {
+        List<Date> dates = searchDocumentWithEventDBAccess.getDatesEvents();
+        List<Integer> retournableDates = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        for (Date date : dates){
+            calendar.setTime(date);
+            retournableDates.add(calendar.get(Calendar.YEAR));
+        }
+
+        return retournableDates;
+    }
+}
