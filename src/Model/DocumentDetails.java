a9e122aa5ac4c4a28a9c66d6ce451827f041dd17
Dorian Michaux <dorianmichaux@MacBook-Pro-de-Dorian.local>
2025-04-03 22:16:57 +0200

    resolve conflict

---
+++ src/Model/DocumentDetails.java
@@ -0,0 +1,52 @@
+package Model;
+
+import Model.Document.Document;
+
+public class DocumentDetails {
+    private Integer id;
+    private String label;
+    private float quantity;
+    private float newQuantity;
+    private float unitPrice;
+    private Document document;
+
+    public DocumentDetails(Integer id, String label, float quantity, float newQuantity,
+                           float unitPrice, Document document)
+    {
+        this.id = id;
+        this.label = label;
+        this.quantity = quantity;
+        this.newQuantity = newQuantity;
+        this.unitPrice = unitPrice;
+        this.document = document;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public float getNewQuantity() {
+        return newQuantity;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + label + " - " + quantity + " - " + newQuantity + " - " + unitPrice + " - " + document;
+    }
+}
